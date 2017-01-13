a4f78391a5b6b69092f2225e51f1b0f17c364591
Aleksandr Sidorov <shtil80@gmail.com>
2017-01-13 14:50:33 +0700

    redux, react-redux and react-router added

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h4>React App</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+
+import App from './components/App'
+import reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
+)
